c5263b7dc8b5ec9a78d2bff5902cb8bc239bae42
John <John@elionics.bsu.by>
2011-09-08 12:37:28 +0300

    minors

--- kernel.js
+++ kernel.js
@@ -1,17 +1,22 @@
 
 //localStorage.kernel_data = 'kernel data';
 
+chrome.browserAction.onClicked.addListener(function(tab) {
+    console.log('You pressed browser action');
+});
 
 chrome.extension.onRequest.addListener(function(req, sender, respond){
 });
 
 function inject(name){
-    chrome.tabs.executeScript(null, {file: name.toString() + '.js'});
+    chrome.tabs.executeScript(null, {
+        file: name.toString() + '.js'
+    });
 }
 
 function open_view (url){
     chrome.tabs.create({
-        url: chrome.extension.getURL(url)
+        url: chrome.extension.getURL(url + '.html')
     });
 }
 
