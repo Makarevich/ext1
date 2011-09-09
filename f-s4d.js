5ce22ffa9dad28c2b810105807d6a79ee69c0ba3
John <John@elionics.bsu.by>
2011-09-09 17:16:26 +0300

    added basic paginator parser

--- f-s4d.js
+++ f-s4d.js
@@ -1,28 +1,60 @@
 
-console.log('Running s4d fetcher');
+(function () {
 
-// request tab url
-chrome.tabs.getSelected(null, function(tab){
-    check_url(tab.url);
-});
+    var base_url;           // base fetching url
+    var initial = {};       // initial page info
 
-function check_url(url){
-    var checker = /^http:\/\/www.scholars4dev.com\//;
+    console.log('Running s4d fetcher');
 
-    if(!checker.test(url)){
-        console.error("URL \"" + url + "\" doesnot match /" + checker.source + "/");
-        return;
+    // request tab url
+    chrome.tabs.getSelected(null, function(tab){
+        check_url(tab.url);
+    });
+
+    function check_url(url){
+        var checker = /^http:\/\/www.scholars4dev.com\//;
+
+        // check url
+        if(!checker.test(url)){
+            console.error("URL \"" + url + "\" doesnot match /" + checker.source + "/");
+            return;
+        }
+
+        // obtain "base url"
+        var m = url.match(/^(.*\/)page\/\d+.$/);
+        if(m){
+            base_url = m[1];
+        }else{
+            base_url = url;
+        }
+
+        // request the page
+        jQuery.get(url).done(parse_first_page);
     }
 
-    jQuery.get(url).done(parse_first_page);
-}
+    function parse_first_page(page_text){
+        // parse the fetched html
+        docroot.innerHTML = page_text;
 
-function parse_first_page(page_text){
-    docroot.innerHTML = page_text;
+        // === parse paginator text ===
+        var content = $('div#content > div > div.maincontent', docroot);
 
-    console.log(
-    $('div#content > div > div.maincontent > div.post', docroot).size()
-    );
+        console.assert(content.length != 1);
 
-}
+        var page_text = content.find('div.wp-pagenavi > span.pages').text();
 
+        var m = page_text.match(/Page (\d+) of (\d+)/);
+
+        if(!m){
+            console.error("Cannot parse paginator text \"" + page_text + "\"");
+            return;
+        }
+
+        initial.num = m[1];
+        initial.html = page_text;
+
+        // var content = $('div#content > div > div.maincontent > div.post', docroot)
+
+    }
+
+})();
