1381ac703c5ccf040e920f0b9b535546af6bc36a
U-John-PC\John <John@home.org>
2011-09-18 20:20:45 +0300

    added hash and keys to display

--- display.js
+++ display.js
@@ -4,8 +4,10 @@ chrome.extension.sendRequest('ready', function(o){
     console.assert(o.key != null);
     console.assert(o.data != null);
 
+    /* printing display hash */
+    $('body').append('<h1>' + o.key + ' (' + sha1(o.data)  + ')' + '</h1>')
+
     $('div#patterns > div').each(function(){
-        console.log(this);
         if(o.key.indexOf(this.id) == 0){
             $(this).removeAttr('id');
 
@@ -21,12 +23,14 @@ chrome.extension.sendRequest('ready', function(o){
         var body = $('body');
 
         // find template node
+        nodes['key'] = pat.find('.key');
         for(var i in data[0]){
             nodes[i] = pat.find('.' + i);
         }
 
         // process data
         for(var i in data){
+            nodes['key'].text(keycode.enkey(i));
             for(var j in data[i]){
                 nodes[j].text(data[i][j]);      // TODO: specify various types of content
             }
