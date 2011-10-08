83831adff1e513538660f3dede688e951c52671c
U-John-PC\John <John@home.org>
2011-10-08 18:57:28 +0200

    elaborated pattern processing algorithm

--- display.js
+++ display.js
@@ -24,11 +24,11 @@ chrome.extension.sendRequest('ready', function(o){
     gen_elements(pat, LZW.decompress(o.data));
 
     function gen_elements(pat, data){
-        var nodes = [];
-
         var body = $('body');
 
         var counter_node = $(document.createElement('h2')).appendTo(body);
+
+        var nodes = {};
 
         // find template node
         nodes['key'] = pat.find('.key');
@@ -39,18 +39,67 @@ chrome.extension.sendRequest('ready', function(o){
         // process data
         var count = 0;
         for(var i in data[any_key(data)]){
+            var nodes_to_remove = [];
+
             nodes['key'].text(keycode.enkey(i));
             for(var j in data){
-                nodes[j].text(data[j][i]);      // TODO: specify various types of content
+                var node = nodes[j];
+                var data_array = data[j];
+
+                var set_node_data = node.hasClass('html')
+                    ? set_node_html : set_node_text;
+
+                if(node.hasClass('array')){
+                    //
+                    // Array nodes imply that the appropriate data
+                    // not a text, but an array (which it traversed with
+                    // a for/in loop). For each element of the array a copy
+                    // of the node is made. This copy is placed AFTER
+                    // the original node, making all the copies the siblings
+                    // of the node.
+                    //
+                    // All copies are removed after the data element is processed.
+                    //
+
+                    console.assert(data_array[i].length,
+                        "Field '" + j + "' of element " + i +
+                        " is not an array");
+            
+                    set_node_data(node, data_array[i][0]);
+
+                    for(var k = 1; k < data_array[i].length; k++){
+                        node = node.clone().insertAfter(node);
+
+                        nodes_to_remove.push(node);
+
+                        set_node_data(node, data_array[i][k]);
+                    }
+                }else{
+                    set_node_data(node, data_array[i]);
+                }
+
             }
             pat.clone().appendTo(body);
             ++count;
+
+            // remove all generated nodes from the pattern
+            for(var j in nodes_to_remove){
+                nodes_to_remove[j].remove();
+            }
         }
 
         counter_node.text(count.toString() + ' posts');
 
         function any_key(o){
             for(var k in o) return k;
+        }
+
+        function set_node_html(jnode, html){
+            jnode.html(html);
+        }
+
+        function set_node_text(jnode, text){
+            jnode.text(text);
         }
     }
 });
